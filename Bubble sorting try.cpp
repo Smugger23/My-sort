240d635634e0d718dfff0a6839708341522e093d
Smugger23 <jo.yauri@gmail.com>
2025-09-02 21:32:11 +0700

    Update Bubble sorting try.cpp

--- Bubble sorting try.cpp
+++ Bubble sorting try.cpp
@@ -4,11 +4,9 @@ int main(){
 	
 	int arr[] = {3, 1, 2, 6, 4, 5, 7, 8};
 	int n = sizeof(arr) / sizeof(arr[0]); 
-	int m = 0;
 	
 	for(int i = 0; i < n; i++){
-		m++;
-		for(int j = i+1; j < n - m; j++){
+		for(int j = i+1; j < n; j++){
 			if(arr[i] > arr[j]){
 				int temp = arr[i];
 				arr[i] = arr[j];
@@ -20,5 +18,6 @@ int main(){
 	
 	return 0;
 }
+
 
 
